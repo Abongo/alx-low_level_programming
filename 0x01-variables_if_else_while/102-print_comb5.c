575383450291c763c9dcffbe01a8890956143961
abongo <abongoje@gmail.com>
2023-07-17 11:36:56 -0700

    12th Print

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,15 +5,15 @@
   * Return: Always 0 (Success)
  */
 int main(void)
-{       
-        int p, q;
-        for (p = 0; p <= 98; p++)
+{
+	int p, q;
+
+	for (p = 0; p <= 98; p++)
 	{
-	
 	for (q = p + 1; q <= 99; q++)
 	{
 	putchar((p / 10) + '0');
-	putchar((p %10) + '0');
+	putchar((p % 10) + '0');
 	putchar(' ');
 	putchar((q / 10) + '0');
 	putchar((q % 10) + '0');
