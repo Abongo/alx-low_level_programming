6f4185051c9b0bcb1a21ac29084f95ca1971b254
abongo <abongoje@gmail.com>
2023-07-17 11:54:31 -0700

    10th Print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,21 +6,19 @@
  */
 int main(void)
 {
-	int n;
-	int m;
+	int n, m;
 
 	for (n = 0; n < 9; n++)
 	{
-		for (m = n + 1; m < 10; m++)
-		{
-			putchar((n % 10) + '0');
-			putchar((m % 10) + '0');
-				if
-					(n == 8 && m == 9);
-				continue;
-				putchar(',');
-				putchar(' ');
-		}
+	for (m = n + 1; m < 10; m++)
+	{
+	putchar((n % 10) + '0');
+	putchar((m % 10) + '0');
+	if (n == 8 && m == 9)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
