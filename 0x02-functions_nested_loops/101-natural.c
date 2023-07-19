081750dd354c14382031356836d3abcc131d1509
abongo <abongoje@gmail.com>
2023-07-19 06:38:44 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the sum
+ * Return: Always (Success)
+ */
+int main(void)
+
+{
+	int a, b = 0;
+
+	while (a < 1024)
+	{
+	if ((a % 3 == 0) || (i % 5 == 0))
+	{
+	b += a;
+	}
+	a++;
+	}
+	printf("%d\n", b);
+	return (0);
+}
