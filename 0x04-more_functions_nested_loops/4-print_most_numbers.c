fabc9f68d16a8bdaef8555202ac721b9933b422a
abongo <abongoje@gmail.com>
2023-07-20 05:08:30 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_most_numbers - check the code
+ *Description: the number to be checked
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	for (; x <= 9; x++)
+	{
+	if (x == 2 || x == 0)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(x + '0');
+	}
+	}
+}
