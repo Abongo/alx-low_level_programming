49e7ea972e75f1592db6c7218013161dcc3ccee1
abongo <abongoje@gmail.com>
2023-07-20 04:36:32 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code.
+ * @x: the number to be checked
+ * Return: Always 0.
+ */
+int _isupper(int x)
+
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
