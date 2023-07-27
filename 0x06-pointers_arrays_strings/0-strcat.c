ba6e769e1f09a4cf07ebf53c2d2565d9b1bcbc5b
abongo <abongoje@gmail.com>
2023-07-27 00:36:55 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,22 +8,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int k;
-	int h;
+	int i;
+	int j;
 
-	k = 0;
-	while (dest[k] != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		k++;
+		i++;
 	}
-	h = 0;
-	while (src[h] != '\0')
+	j = 0;
+	while (src[j] != '\0')
 	{
-		dest[k] = src[h];
-		k++;
-		h++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 
-	dest[k] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
+
