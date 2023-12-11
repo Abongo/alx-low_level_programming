47f19649e903cc21cfd81fc595e14309a44ed5b9
abongo <abongoje@gmail.com>
2023-12-11 12:41:31 -0800

    Juke

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Abongo/alx-low_level_programming/b507289b8a471278af42161431aac5c21ffd1c1e/0x18-dynamic_libraries/libcyber.so
-export LD_PRELOAD="$PWD/../libcyber.so"
+wget -P .. https://raw.githubusercontent.com/Abongo/alx-low_level_programming/master/0x18-dynamic_libraries/libcyber1.so
+export LD_PRELOAD="$PWD/../libcyber1.so"
